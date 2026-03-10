2cfe42dc482eedfb2e215e07ef58266f76d36825
jinaganunperson <06younseo@gmail.com>
2026-03-10 22:57:26 +0900

    색깔 바꾸기, 외부 링크, 사진 바꾸기 연습

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -69,7 +69,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(506, 235);
+            this.button3.Location = new System.Drawing.Point(484, 223);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(159, 54);
             this.button3.TabIndex = 3;
