c1e2e5c98e668afbad8866254d565471259487bd
jinaganunperson <06younseo@gmail.com>
2026-03-10 22:56:04 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.p1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            // 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/jinaganunperson");
+        }
+    }
+}
